e176b5f2467c5a9f91e9fcbca325fdb60782e207
Tran Van Truong <truong.tran0813@gmail.com>
2024-05-08 15:09:01 +0700

    chore: Update library_app module summary

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,9 @@
 {
     "name": "Library Management",
-    "summary": "Manage library catalog and book lending.",
+    "summary": "Test1",
     "author": "Daniel Reis",
     "license": "AGPL-3",
-    "website": "https://github.com/PacktPublishing"
-               "/Odoo-14-Development-Essentials",
+    "website": "https://github.com/PacktPublishing" "/Odoo-14-Development-Essentials",
     "version": "14.0.1.0.0",
     "category": "Services/Library",
     "depends": ["base"],
@@ -16,7 +15,7 @@
         "views/book_list_template.xml",
         "reports/library_book_report.xml",
         "reports/library_publisher_report.xml",
-        ],
+    ],
     "demo": [
         "data/res.partner.csv",
         "data/library.book.csv",
